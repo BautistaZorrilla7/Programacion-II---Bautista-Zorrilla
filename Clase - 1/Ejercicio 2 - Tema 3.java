9cf17bfe33a58316987f7b38e1016beef53099cc
BautistaZorrilla7 <bautistazorrillaa@gmail.com>
2026-09-23 16:27:51 -0300

    Clase 1: Agrega Ejercicio 2 - Tema 3

---
+++ Clase - 1/Ejercicio 2 - Tema 3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class VerificadorPrimo {
+
+    public static boolean esPrimo(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        boolean primo = true;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                primo = false;
+                break;
+            }
+        }
+        return primo;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese un número entero: ");
+        int n = sc.nextInt();
+
+        if (esPrimo(n)) {
+            System.out.println("El número es primo.");
+        } else {
+            System.out.println("El número no es primo.");
+        }
+
+        sc.close();
+    }
+}
