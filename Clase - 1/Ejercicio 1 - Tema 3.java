a2c4439ba180bf7d123dfde741c64190b6066a1b
BautistaZorrilla7 <bautistazorrillaa@gmail.com>
2026-09-23 16:26:40 -0300

    Clase 1: Agrega Ejercicio 1 - Tema 3

---
+++ Clase - 1/Ejercicio 1 - Tema 3.java
@@ -0,0 +1,10 @@
+/*
+ * Verificación de versiones desde la terminal:
+ * java version "17.0.x" 
+ * javac version "17.0.x"
+ */
+public class HolaProgramacionII {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, bienvenido a Programación II!");
+    }
+}
