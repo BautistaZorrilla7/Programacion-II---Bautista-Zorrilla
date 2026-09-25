7378772ab84672e006462abb2a56d8653ad407b5
BautistaZorrilla7 <bautistazorrillaa@gmail.com>
2026-09-25 15:38:52 -0300

    Clase - 6: Agrega subclase Rectangulo.java

---
+++ Clase - 6/Rectangulo.java
@@ -0,0 +1,31 @@
+public class Rectangulo extends Figura {
+    private double base;
+    private double altura;
+
+    public Rectangulo(double base, double altura) {
+        super("Rectángulo");
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return this.base * this.altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
