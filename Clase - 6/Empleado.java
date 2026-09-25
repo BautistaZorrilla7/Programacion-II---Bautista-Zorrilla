58dab1797884738f1f522db9b84b079d9bda3e22
BautistaZorrilla7 <bautistazorrillaa@gmail.com>
2026-09-25 15:36:14 -0300

    Clase - 6: Agrega clase base Empleado para la jerarquía

---
+++ Clase - 6/Empleado.java
@@ -0,0 +1,29 @@
+public class Empleado {
+    protected String nombre;
+    protected int legajo;
+
+    public Empleado(String nombre, int legajo) {
+        this.nombre = nombre;
+        this.legajo = legajo;
+    }
+
+    public double calcularSueldo() {
+        return 0.0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+}
